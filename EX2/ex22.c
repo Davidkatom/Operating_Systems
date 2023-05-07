03a305377b4375eabcad920fa16d5fb39c796c2b
David <Davidkatom@gmail.com>
2023-05-08 01:54:23 +0300

    exit status working

--- EX2/ex22.c
+++ EX2/ex22.c
@@ -9,12 +9,13 @@
 
 #define TIMEOUT 5
 
+// Timeout handler for the alarm signal
 void alarm_handler(int signo) {
-    printf("Child process timed out - singal %d\n", signo);
-    //killpg(getpid(), SIGKILL);
+    printf("Child process timed out - signal %d\n", signo);
     exit(4);
 }
 
+// Checks if the given folder contains a C source file
 int contains_c_file(const char *folder_path) {
     DIR *folder = opendir(folder_path);
     if (!folder) {
@@ -38,19 +39,19 @@ int contains_c_file(const char *folder_path) {
     return c_file_found;
 }
 
-
 int main(int argc, char *argv[]) {
+    // Check for correct number of arguments
     if (argc != 2) {
         printf("Usage: %s <configuration_file>\n", argv[0]);
         return 1;
     }
 
+    // Open and read configuration file
     FILE *config_file = fopen(argv[1], "r");
     if (!config_file) {
         perror("Error opening configuration file\n");
         return 1;
     }
-
 
     char folder_path[1024], input_file[1024], correct_output_file[1024];
     fgets(folder_path, sizeof(folder_path), config_file);
@@ -64,39 +65,35 @@ int main(int argc, char *argv[]) {
 
     fclose(config_file);
 
-    //printf("folder - %s\n", folder_path);
-    //printf("input - %s\n", input_file);
-    //printf("output - %s\n", correct_output_file);
-
-
+    // Open the main folder
     DIR *folder = opendir(folder_path);
     if (!folder) {
         perror("Error opening folder\n");
         return 1;
     }
 
-
     struct dirent *entry;
 
-
+    // Iterate through the subfolders
     while ((entry = readdir(folder)) != NULL) {
         int exit_status = 0;
-
 
         if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
             char subfolder_path[1024];
             snprintf(subfolder_path, sizeof(subfolder_path), "%.254s/%.254s", folder_path, entry->d_name);
-            printf("%s\n\n\n\n\n", subfolder_path);
+
+            // Check if the subfolder contains a C source file
             if (contains_c_file(subfolder_path) == 0) {
                 printf("No C file\n");
                 continue;
             }
 
+
+            // Create a child process to run the program
             pid_t pid = fork();
             if (pid == 0) {
-                int exit_s = 0;
-
-
+                // Child process
+                // Redirect input, output, and error streams
                 FILE *input_fp = fopen(input_file, "r");
                 if (!input_fp) {
                     fprintf(stderr, "Error opening input file (%s): %s\n", input_file, strerror(errno));
@@ -120,20 +117,18 @@ int main(int argc, char *argv[]) {
                 fclose(errors_fp);
                 chdir(subfolder_path);
 
+                printf("\n\n\n\n%s\n", subfolder_path);
+
                 pid_t monitor_pid = fork();
                 if (monitor_pid > 0) {
+                    // Parent of the monitor process
                     int s;
                     signal(SIGALRM, alarm_handler);
                     alarm(TIMEOUT);
                     waitpid(monitor_pid, &s, 0);
-                    //if (WIFSIGNALED(s) && WTERMSIG(s) == SIGALRM) {
-                    //    compare_output = 0; // Set the flag to 0 in case of a timeout
-                    //}
                 } else {
-
-
+                    // Monitor process
                     char compile_command[1024];
-                    //TODO no printf
                     snprintf(compile_command, sizeof(compile_command), "gcc *.c -o program");
 
                     if (system(compile_command) == 0) {
@@ -143,21 +138,21 @@ int main(int argc, char *argv[]) {
                         execl("./program", "program", NULL);
                     } else {
                         perror("Error compiling C program\n");
-                        exit_status = 3; // Set the exit_status to 4 in case of a timeout
                         exit(3);
                     }
-
                 }
-                printf("status = %d", exit_status);
+                int status;
+                waitpid(monitor_pid, &status, 0);
+                exit(WEXITSTATUS(status));
             } else if (pid < 0) {
                 perror("Error creating process");
             } else {
+                // Parent process
                 int status;
                 waitpid(pid, &status, 0);
 
                 if (WIFEXITED(status)) {
                     int exit_status = WEXITSTATUS(status);
-                    printf("exit status - %d\n", exit_status);
                     if (exit_status == 3) {
                         printf("Program in %s failed to compile\n", subfolder_path);
                         continue; // Skip this iteration and move on to the next directory
@@ -166,16 +161,17 @@ int main(int argc, char *argv[]) {
 
                 pid_t comp_pid = fork();
                 if (comp_pid == 0) {
+                    // Child process to execute comp.out
                     execl("./comp.out", "comp.out", "temp_output.txt", correct_output_file, NULL);
                     perror("Error executing comp.out");
                     exit(1);
                 } else if (comp_pid < 0) {
                     perror("Error creating comp.out process");
                 } else {
-                    if(WIFEXITED(status) == 0)
-                        continue;
+                    // Parent process for comp.out
                     int comp_status;
                     waitpid(comp_pid, &comp_status, 0);
+
                     if (WIFEXITED(comp_status)) {
                         int exit_status = WEXITSTATUS(comp_status);
                         if (exit_status == 1) {
@@ -196,3 +192,4 @@ int main(int argc, char *argv[]) {
     closedir(folder);
     return 0;
 }
+
