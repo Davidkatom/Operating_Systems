3cf3233f8f65562ede1f22daf7e31027b5426a72
David <Davidkatom@gmail.com>
2023-05-29 19:15:25 +0300

    DONE

--- EX3/main.c
+++ EX3/main.c
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
 
     fclose(config);
 
-//    free(producers);
+      free(producers);
 //    free(producerThreads);
 //    free(dispatcher->sportsEditor->buffer);
 //    free(dispatcher->weatherEditor->buffer);
